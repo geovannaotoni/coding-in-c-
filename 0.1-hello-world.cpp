c6a89afacb1552f090097895e2f79663fdbe1f48
Geovanna Otoni <geovannaotoni@gmail.com>
2024-03-10 12:11:39 -0300

    Create 0.1-hello-world.cpp

---
+++ 0.1-hello-world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string hello_world = "Hello World!";
+    string matricula = "1234567";
+    
+    cout << hello_world;
+    cout << endl;
+    cout << matricula;
+}
