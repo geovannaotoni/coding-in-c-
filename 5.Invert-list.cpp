813e415cddb4f3fdffd81246297e1fdf058c1008
Geovanna Otoni <geovannaotoni@gmail.com>
2024-04-18 21:11:48 -0300

    Create 5.Invert-list.cpp

---
+++ 5.Invert-list.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "list.hpp"
+
+void List::insertNode(int data) {
+  Node* aux = new Node;
+  aux->data = data;
+  aux->next = nullptr;
+  if (head == nullptr) {
+    head = aux;
+    tail = aux;
+  } else {
+    tail->next = aux;
+    tail = aux;
+  }
+}
+
+void List::display() {
+  Node *aux = head;
+  while (aux != nullptr) {
+    cout << aux->data << " ";
+    aux = aux->next;
+  }
+  cout << endl;
+}
+
+// Você deverá implementar apenas esse método
+void List::inverte(){
+}
